2ed85edf486966dab81bb2889bab3dd1f7401113
Thyge Steffensen <s175176@student.dtu.dk>
2018-02-22 21:41:43 +0100

    + IUser

---
+++ CDIO1/src/funk/IUser.java
@@ -0,0 +1,10 @@
+package funk;
+
+import dal.IUserDAO.DALException;
+
+public interface IUser {
+	String showUsers() throws DALException;
+	String createUser(String name, String cpr, String role) throws DALException; // returns info, of the newly added user 
+	String updateUser(String type, String change) throws DALException; // returns info, of the recently changed user
+	void deleteUser() throws DALException;
+}
